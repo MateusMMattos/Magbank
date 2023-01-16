50ade546092248ab84c77912ea1caa689bb79da8
Mateus Mattos <mateusmattos6@gmail.com>
2023-01-16 14:39:54 -0300

    estilizando navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,36 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Form, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container, Button, ButtonGroup} from 'react-bootstrap';
 import './Navbar.scss';
 
 const Navigation = () => (
     <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
+        <Container fluid>
+            <Navbar.Brand href="#">MAGBANK</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#cartao">Cartão</Nav.Link>
+                    <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
+                    <Nav.Link href="#faq">FAQ</Nav.Link>
+                </Nav>
+                <ButtonGroup aria-label="Basic example">
+                    <Button variant="outline-secondary">
+                        <NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                            Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                            Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Button>
+                    <Button variant="outline-secondary">abra sua conta</Button>
+                </ButtonGroup>
+            </Navbar.Collapse>
+        </Container>
     </Navbar>
 );
 
