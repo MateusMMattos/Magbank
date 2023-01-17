e996c20e71efc7b21904b8c433e859170833f934
Mateus Mattos <mateusmattos6@gmail.com>
2023-01-17 09:21:34 -0300

    Estruturando o Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar.js'
 import './App.scss';
+import Hero from './components/Hero.js';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
