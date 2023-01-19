409f4712eef86514dc3992748aee22b427698976
Mateus Mattos <mateusmattos6@gmail.com>
2023-01-19 10:37:55 -0300

    adicionado hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Button, Row, Col, Card, Image } from 'react-bootstrap';
+import logo from '../assets/logo__magic-pay--mobile.png';
+import logoDesktop from '../assets/logo__magic-pay.png';
 
 const Hero = () => (
-    <Jumbotron>
-        <p>Pague suas contas pelo nosso APP</p>
-        <Button variant="outline-light">Abra sua conta</Button>
-    </Jumbotron>
+    <Card className='text-center text-light hero'>
+        <Row className='my-lg-5 d-flex gap-5 hero__row'>
+            <Col lg className="text-lg-end my-5">
+                <Image className='d-lg-none text-center' src={logo}/>
+                <Image className='d-none d-lg-inline-block hero__logo' src={logoDesktop}/>
+            </Col>
+            <Col lg className="text-lg-start my-5">
+                <p>Pague suas contas pelo nosso APP</p>
+                <Button variant="outline-light">Abra sua conta</Button>
+            </Col>
+        </Row>
+    </Card>
 );
 
 export default Hero;
