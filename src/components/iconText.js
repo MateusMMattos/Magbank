bf6ec83d4a1480af073644fac62b472b82ad2439
Mateus Mattos <mateusmattos6@gmail.com>
2023-02-22 14:34:27 -0300

    estilizado textos do institutional

--- src/components/iconText.js
+++ src/components/iconText.js
@@ -3,44 +3,40 @@ import { Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMobileButton, faMobileScreenButton, faGlobe, faShieldHalved } from '@fortawesome/free-solid-svg-icons';
 
+import './iconText.scss';
+
 const IconText = () => (
   <Row>
     <Col xs={12} lg={6} className="mb-4">
-      <Row>
-        <Col xs={2} className="d-flex justify-content-center">
-          <FontAwesomeIcon icon={faMobileScreenButton} size="2x" color="#ffff" />
+      <Row className='py-3'>
+        <Col xs={2} className="d-flex justify-content-center icon_Institutional">
+          <FontAwesomeIcon icon={faMobileScreenButton} color="#ffff" />
         </Col>
-        <Col xs={10} className="h5 text-muted">
+        <Col xs={10} className="h6 px-0 text_institutional" color='#ffff'>
           Sem fila e sem burocracia
         </Col>
       </Row>
-    </Col>
-    <Col xs={12} lg={6} className="mb-4">
-      <Row>
-        <Col xs={2} className="d-flex justify-content-center">
-          <FontAwesomeIcon icon={faMobileButton} size="2x" color="#ffff" />
+      <Row className='py-3'>
+        <Col xs={2} className="d-flex justify-content-center icon_Institutional">
+          <FontAwesomeIcon icon={faMobileButton} color="#ffff" />
         </Col>
-        <Col xs={10} className="h5 text-muted">
+        <Col xs={10} className="h6 px-0 text_institutional" color='#ffff'>
           Simples e prático
         </Col>
       </Row>
-    </Col>
-    <Col xs={12} lg={6} className="mb-4">
-      <Row>
-        <Col xs={2} className="d-flex justify-content-center">
-          <FontAwesomeIcon icon={faGlobe} size="2x" color="#ffff" />
+      <Row className='py-3'>
+        <Col xs={2} className="d-flex justify-content-center icon_Institutional">
+          <FontAwesomeIcon icon={faGlobe} color="#ffff" />
         </Col>
-        <Col xs={10} className="h5 text-muted">
+        <Col xs={10} className="h6 px-0 text_institutional" color='#ffff'>
           Abertura de conta 100% online
         </Col>
       </Row>
-    </Col>
-    <Col xs={12} lg={6} className="mb-4">
-      <Row>
-        <Col xs={2} className="d-flex justify-content-center">
-          <FontAwesomeIcon icon={faShieldHalved} size="2x" color="#ffff" />
+      <Row className='py-3'>
+        <Col xs={2} className="d-flex justify-content-center icon_Institutional">
+          <FontAwesomeIcon icon={faShieldHalved} color="#ffff"/>
         </Col>
-        <Col xs={10} className="h5 text-muted">
+        <Col xs={10} className="h6 px-0 text_institutional" color='#ffff'>
           Transações mais seguras
         </Col>
       </Row>
--- src/components/institutional.js
+++ src/components/institutional.js
@@ -11,12 +11,12 @@ const institutional = () => (
                 <Col xs={12} lg={5}/>
                 <Col xs={12} lg={7}>
                     <h2 className='institutional-title my-5'>Já nascemos digital</h2>
-                    <p className='mb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lorem odio, scelerisque nec felis ac, dapibus tempor risus. Cras viverra fringilla urna, id placerat nulla efficitur ac. Nulla sit amet mollis leo, at finibus nunc. Vestibulum nisi nisi, iaculis sit amet cursus vitae, gravida et enim. Curabitur ac congue lacus. Donec in luctus arcu. Etiam efficitur ac tellus non elementum. Proin in dui leo. Quisque lacinia hendrerit placerat. Curabitur faucibus ante vitae neque tincidunt euismod.</p>
+                    <p className='mb-5 description_Institutional'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lorem odio, scelerisque nec felis ac, dapibus tempor risus. Cras viverra fringilla urna, id placerat nulla efficitur ac. Nulla sit amet mollis leo, at finibus nunc. Vestibulum nisi nisi, iaculis sit amet cursus vitae, gravida et enim. Curabitur ac congue lacus. Donec in luctus arcu. Etiam efficitur ac tellus non elementum. Proin in dui leo. Quisque lacinia hendrerit placerat. Curabitur faucibus ante vitae neque tincidunt euismod.</p>
                     
                     <IconText />
 
-                    <Button className='mt-5'
-                    variant='outline-light'>
+                    <Button className='mt-5 px-4'
+                    variant='outline-light' size='lg'>
                         Abra sua conta
                     </Button>
                 </Col>
